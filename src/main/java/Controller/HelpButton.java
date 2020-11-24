df7d5eb9fb07b689235b246cca53eeda26516f8f
haox1an <chunghaoxian@gmail.com>
2020-11-25 01:56:56 +0800

    Created HelpButton

---
+++ src/main/java/Controller/HelpButton.java
@@ -0,0 +1,83 @@
+package Controller;
+
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+
+
+public class HelpButton extends Button {
+    private final int width = 450;
+    private final int height =1000;
+
+    ImageView HelpButtonPressed = new ImageView(new Image("file:src/main/resources/Img/HelpButton.png", width, height, true, true));
+    ImageView HelpButtonReleased = new ImageView(new Image("file:src/main/resources/Img/HelpButton.png", width, height, true, true));
+
+
+
+    private final String Button_Style1 = "-fx-background-color: transparent";
+
+    public HelpButton(double Xpos, double Ypos)
+    {
+        setGraphic(HelpButtonReleased);
+        setStyle(Button_Style1);
+        setLayoutX(Xpos);
+        setLayoutY(Ypos);
+        initializeButtonListener();
+    }
+
+    private void setButtonPressedStyle(){
+        setGraphic(HelpButtonPressed);
+
+    }
+
+    private void setButtonReleasedStyle(){
+        setGraphic(HelpButtonReleased);
+
+    }
+
+
+    private void initializeButtonListener() {
+        setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                if(event.getButton().equals(MouseButton.PRIMARY))
+                {
+                    setButtonPressedStyle();
+                }
+            }
+        });
+
+        setOnMouseReleased(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                if(event.getButton().equals(MouseButton.PRIMARY))
+                {
+                    setButtonReleasedStyle();
+                }
+            }
+        });
+
+
+        setOnMouseEntered(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                {
+                    setButtonPressedStyle();
+                }
+            }
+        });
+
+
+        setOnMouseExited(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                {
+                    setButtonReleasedStyle();
+                }
+            }
+        });
+    }
+}
