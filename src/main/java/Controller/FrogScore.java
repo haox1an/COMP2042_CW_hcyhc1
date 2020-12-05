77965bbaf45861acbc096e8a351555efb18ff142
haox1an <chunghaoxian@gmail.com>
2020-12-05 23:01:34 +0800

    Added Music Class

--- src/main/java/Controller/FrogScore.java
+++ src/main/java/Controller/FrogScore.java
@@ -1,15 +1,6 @@
 package Controller;
 
 public class FrogScore{
-    int[] frogScore = new int[10];
-    public FrogScore() {};
 
-    public void setFrogScore(int level, int score){
-        frogScore[level] = score;
-    };
-
-    public int getFrogScore(int level){
-        return frogScore[level];
-    }
 
 }
---
+++ src/main/java/Model/Music.java
@@ -0,0 +1,24 @@
+package Model;
+
+import java.io.File;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.util.Duration;
+
+
+public class Music {
+
+    MediaPlayer mediaPlayer;
+    public static final String MUSIC_PATH = "src/main/resources/Media/FroggerMedia.mp3";
+    public Music() {
+
+    }
+
+    public void playMusic() {
+        String musicFile = MUSIC_PATH;
+        Media sound = new Media(new File(MUSIC_PATH).toURI().toString());
+        mediaPlayer = new MediaPlayer(sound);
+        mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+        mediaPlayer.play();
+    }
+}
